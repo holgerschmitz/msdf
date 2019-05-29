19764aa1d780ec97c7b2f4fcb351240c5afaa564
Holger Schmitz <holger.schmitz@stfc.ac.uk>
2019-05-29 12:31:06 +0100

    Completed docs and testing of binary IO

--- src/common/binaryio.hpp
+++ src/common/binaryio.hpp
@@ -49,8 +49,9 @@ namespace msdf {
     inline void readString(std::istream &in, std::string &str, size_t length)
     {
       assert(length<1024);
-      char buffer[1024];
+      char buffer[1025];
       in.read(buffer,length);
+      buffer[length] = 0;
       str = std::string(buffer);
       boost::trim(str);
     }
@@ -68,6 +69,7 @@ namespace msdf {
        */
       std::string name;
     public:
+
       /**
        * Constructor that takes the name of the data block
        *
@@ -101,68 +103,173 @@ namespace msdf {
       { return name;  }
   };
 
+  /**
+   * An exception that is thrown when a block type is not supported
+   */
   class BlockTypeUnsupportedException
   {
     private:
+
+      /**
+       * The name of the block
+       */
       std::string blockname;
+
+      /**
+       * The type of the block
+       */
       std::string blocktype;
     public:
+
+      /**
+       * Constructor that takes the name and type of the data block
+       *
+       * @param blockname_  the name of the block
+       * @param blocktype_  the type of the block
+       */
       BlockTypeUnsupportedException(std::string blockname_, std::string blocktype_) throw()
           : blockname(blockname_), blocktype(blocktype_) {}
 
+      /**
+       * Copy constructor
+       *
+       * @param ex  the exception to copy from
+       */
       BlockTypeUnsupportedException(const BlockTypeUnsupportedException &ex) throw()
           : blockname(ex.blockname), blocktype(ex.blocktype) {}
 
+      /**
+       * Assignment operator
+       *
+       * @param ex  the exception to copy from
+       */
       BlockTypeUnsupportedException &operator=(const BlockTypeUnsupportedException &ex) throw()
       { blockname=ex.blockname; blocktype=ex.blocktype;  return *this; }
 
+      /**
+       * Get the name of the block that caused the exception
+       *
+       * @return  the name of the block
+       */
       std::string getBlockName() const throw()
       { return blockname;  }
 
+      /**
+       * Get the type of the block that caused the exception
+       *
+       * @return  the type of the block
+       */
       std::string getBlockType() const throw()
       { return blocktype;  }
   };
 
 
+  /**
+   * An exception that is thrown when a data type is not supported
+   */
   class DataTypeUnsupportedException
   {
     private:
+
+      /**
+       * The name of the block
+       */
       std::string blockname;
+
+      /**
+       * The data type of the block
+       */
       std::string datatype;
     public:
+
+      /**
+       * Constructor that takes the name and data type of the block
+       *
+       * @param blockname_  the name of the block
+       * @param datatype_  the data type of the block
+       */
       DataTypeUnsupportedException(std::string blockname_, std::string datatype_) throw()
           : blockname(blockname_), datatype(datatype_) {}
 
+      /**
+       * Copy constructor
+       *
+       * @param ex  the exception to copy from
+       */
       DataTypeUnsupportedException(const DataTypeUnsupportedException &ex) throw()
           : blockname(ex.blockname), datatype(ex.datatype) {}
 
+      /**
+       * Assignment operator
+       *
+       * @param ex  the exception to copy from
+       */
       DataTypeUnsupportedException &operator=(const DataTypeUnsupportedException &ex) throw()
       { blockname=ex.blockname; datatype=ex.datatype;  return *this; }
 
+      /**
+       * Get the name of the block that caused the exception
+       *
+       * @return  the name of the block
+       */
       std::string getBlockName() const throw()
       { return blockname;  }
 
+      /**
+       * Get the data type of the block that caused the exception
+       *
+       * @return  the data type of the block
+       */
       std::string getDataType() const throw()
       { return datatype;  }
   };
 
+  /**
+   * A generic exception
+   */
   class GenericException
   {
     private:
+
+      /**
+       * The message
+       */
       std::string message;
     public:
+
+      /**
+       * Constructor that takes the message
+       *
+       * @param message_  the human readable message
+       */
       GenericException(std::string message_) throw()
           : message(message_) {}
 
+      /**
+       * Copy constructor
+       *
+       * @param ex  the exception to copy from
+       */
       GenericException(const GenericException &ex) throw()
           : message(ex.message) {}
 
+      /**
+       * Assignment operator
+       *
+       * @param ex  the exception to copy from
+       */
       GenericException &operator=(const GenericException &ex) throw()
       { message = ex.message; return *this; }
 
+      /**
+       * Get the exception message
+       *
+       * @return  the message
+       */
       std::string getMessage() const throw()
       { return message;  }
   };
-}
+
+} // namespace msdf
 
 #endif /* BINARYIO_H_ */
--- test/common/binaryio_spec.cpp
+++ test/common/binaryio_spec.cpp
@@ -9,11 +9,132 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <sstream>
+#include <string>
+
 BOOST_AUTO_TEST_SUITE( range )
 
-BOOST_AUTO_TEST_CASE( read_value )
+BOOST_AUTO_TEST_CASE( readValue_int )
+{
+  std::stringstream buffer;
+
+  int num = 1234567890;
+  buffer.write((char const*) &num, sizeof(num));
+
+  int dest;
+  msdf::detail::readValue(buffer, dest);
+  BOOST_CHECK_EQUAL(dest, num);
+}
+
+BOOST_AUTO_TEST_CASE( readValue_double )
+{
+  std::stringstream buffer;
+
+  double num = 3.14159;
+  buffer.write((char const*) &num, sizeof(num));
+
+  double dest;
+  msdf::detail::readValue(buffer, dest);
+  BOOST_CHECK_EQUAL(dest, num);
+}
+
+BOOST_AUTO_TEST_CASE( readString )
+{
+  std::string str = "foo-bar-baz-qux";
+  std::istringstream buffer(str);
+
+  std::string dest;
+  msdf::detail::readString(buffer, dest, str.size());
+  BOOST_CHECK_EQUAL(dest, str);
+}
+
+BOOST_AUTO_TEST_CASE( readString_clipped )
+{
+  std::string str = "foo-bar-baz-qux";
+  std::istringstream buffer(str);
+
+  std::string dest;
+  msdf::detail::readString(buffer, dest, 3);
+  BOOST_CHECK_EQUAL(dest, "foo");
+}
+
+BOOST_AUTO_TEST_CASE( block_not_found_exception_constructor )
 {
   const msdf::BlockNotFoundException ex("foo");
+  BOOST_CHECK_EQUAL(ex.getName(), "foo");
+
+  const msdf::BlockNotFoundException ex2(ex);
+  BOOST_CHECK_EQUAL(ex2.getName(), "foo");
 }
+
+BOOST_AUTO_TEST_CASE( block_not_found_exception_assign)
+{
+  const msdf::BlockNotFoundException ex("foo");
+  msdf::BlockNotFoundException ex2("bar");
+
+  ex2 = ex;
+  BOOST_CHECK_EQUAL(ex2.getName(), "foo");
+}
+
+BOOST_AUTO_TEST_CASE( block_type_unsupported_exception_constructor )
+{
+  const msdf::BlockTypeUnsupportedException ex("foo", "bar");
+  BOOST_CHECK_EQUAL(ex.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex.getBlockType(), "bar");
+
+  const msdf::BlockTypeUnsupportedException ex2(ex);
+  BOOST_CHECK_EQUAL(ex2.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex2.getBlockType(), "bar");
+}
+
+BOOST_AUTO_TEST_CASE( block_type_unsupported_exception_assign)
+{
+  const msdf::BlockTypeUnsupportedException ex("foo", "bar");
+  msdf::BlockTypeUnsupportedException ex2("baz", "qux");
+
+  ex2 = ex;
+  BOOST_CHECK_EQUAL(ex2.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex2.getBlockType(), "bar");
+}
+
+BOOST_AUTO_TEST_CASE( data_type_unsupported_exception_constructor )
+{
+  const msdf::DataTypeUnsupportedException ex("foo", "bar");
+  BOOST_CHECK_EQUAL(ex.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex.getDataType(), "bar");
+
+  const msdf::DataTypeUnsupportedException ex2(ex);
+  BOOST_CHECK_EQUAL(ex2.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex2.getDataType(), "bar");
+}
+
+BOOST_AUTO_TEST_CASE( data_type_unsupported_exception_assign)
+{
+  const msdf::DataTypeUnsupportedException ex("foo", "bar");
+  msdf::DataTypeUnsupportedException ex2("baz", "qux");
+
+  ex2 = ex;
+  BOOST_CHECK_EQUAL(ex2.getBlockName(), "foo");
+  BOOST_CHECK_EQUAL(ex2.getDataType(), "bar");
+}
+
+BOOST_AUTO_TEST_CASE( generic_exception_constructor )
+{
+  const msdf::GenericException ex("foo");
+  BOOST_CHECK_EQUAL(ex.getMessage(), "foo");
+
+  const msdf::GenericException ex2(ex);
+  BOOST_CHECK_EQUAL(ex2.getMessage(), "foo");
+}
+
+BOOST_AUTO_TEST_CASE( generic_exception_assign)
+{
+  const msdf::GenericException ex("foo");
+  msdf::GenericException ex2("bar");
+
+  ex2 = ex;
+  BOOST_CHECK_EQUAL(ex2.getMessage(), "foo");
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
